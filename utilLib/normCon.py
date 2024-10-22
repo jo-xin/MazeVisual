554c295272d9d26f9f45539a10f9faa9604f587e
jo_xin <16635180001@163.com>
2024-10-22 09:28:31 +0800

    A empty repo is established

---
+++ utilLib/normCon.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Project : MazeVisual
+# @Time    : 2024/10/22 9:23
+# @Author  : jo-xin
+# @File    : normCon.py
+
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
