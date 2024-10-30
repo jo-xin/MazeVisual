ecf9c0711990b108fc319c68829db29fb713593b
jo_xin <16635180001@163.com>
2024-10-30 12:10:55 +0800

    test in ursina

---
+++ tutel/New_World.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Project : MazeVisual
+# @Time    : 2024/10/30 11:02
+# @Author  : jo-xin
+# @File    : New_World.py
+
+import ursina
+
+
+
+if __name__ == '__main__':
+    app = ursina.Ursina()
+    app.run()
+
+
---
+++ tutel/entrance.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Project : MazeVisual
+# @Time    : 2024/10/30 12:07
+# @Author  : jo-xin
+# @File    : entrance.py
+
+from ursina import Ursina
+
+application = Ursina()
+
