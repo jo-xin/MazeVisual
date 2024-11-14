3f35536ad7d563ab23f9e7c210d94a0613f76923
HWeng302 <162140284+HWeng302@users.noreply.github.com>
2024-11-14 23:59:40 +0800

    将dfs各变量标注了，同时添加了未测试的history

--- MazeSolution/Dfs.py
+++ MazeSolution/Dfs.py
@@ -1,14 +1,18 @@
+import queue
 from Maze_base import BasicMaze
 
 
-def dfs(maze,new_x,new_y,last_x,last_y,queue=None):
+def dfs(maze:BasicMaze,new_x:int,new_y:int,last_x:int,last_y:int,history:queue.Queue=None):
     max_length = maze.x_lim*maze.y_lim
     if new_x == maze.target[0] and new_y == maze.target[1]:
         # print("find target")
         # print(new_x,new_y)
+        history.put((-1,-1)) # 找到了一个解
         return 1
     maze.visited[new_x][new_y] = True
     # add queue
+    if history is not None:
+        history.put((new_x,new_y))
     for dx,dy in maze.directions:
         if dx+new_x == last_x and dy+new_y == last_y:
             continue
@@ -16,17 +20,21 @@ def dfs(maze,new_x,new_y,last_x,last_y,queue=None):
         next_y = dy+new_y
         if maze.is_valid_position(next_x, next_y) and not maze.is_wall(next_x,next_y):
             if not maze.is_visited(next_x, next_y):
-                max_length = min(max_length,dfs(maze,next_x,next_y,new_x,new_y,queue))
+                max_length = min(max_length,dfs(maze,next_x,next_y,new_x,new_y,history))
     maze.visited[new_x][new_y] = False
     # add queue
+    if history is not None:
+        history.put((new_x,new_y))
     return max_length+1
 
 
 
-def dfs_sol(maze):
+def dfs_sol(maze:BasicMaze,history:queue.Queue=None):
     maze.visited[maze.origin[0]][maze.origin[1]] = True
     max_length = -1
     # add queue
+    if history is not None:
+        history.put(maze.origin)
     for dx, dy in maze.directions:
         next_x = dx + maze.origin[0]
         next_y = dy + maze.origin[1]
